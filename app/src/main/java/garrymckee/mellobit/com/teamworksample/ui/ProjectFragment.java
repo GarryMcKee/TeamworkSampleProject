a1c0548d11eb7a2ef75c48b37b4e3b2b6f47eb50
GarryMcKee <gmk0232@gmail.com>
2017-06-19 22:02:15 +0100

    Set ActionBar title in project detail view

--- app/src/main/java/garrymckee/mellobit/com/teamworksample/ui/ProjectFragment.java
+++ app/src/main/java/garrymckee/mellobit/com/teamworksample/ui/ProjectFragment.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
@@ -75,6 +76,8 @@ public class ProjectFragment extends Fragment implements ProjectContract.Project
             Project project = mPresenter.getProject(mProjectId);
             String projectName = project.getName();
             String projectDesc = project.getDescription();
+
+            getActivity().setTitle(projectName);
 
             mNameTextview.setText(projectName);
             mDescTextView.setText(projectDesc);
