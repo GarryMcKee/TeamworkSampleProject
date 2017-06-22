014c3995cefc4f73d06111fe0503e277adb5e47f
GarryMcKee <gmk0232@gmail.com>
2017-06-22 22:17:09 +0100

    Remove empty onCreate methods

--- app/src/main/java/garrymckee/mellobit/com/teamworksample/projectdetail/ProjectFragment.java
+++ app/src/main/java/garrymckee/mellobit/com/teamworksample/projectdetail/ProjectFragment.java
@@ -55,11 +55,6 @@ public class ProjectFragment extends Fragment implements ProjectContract.Project
         return fragment;
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
--- app/src/main/java/garrymckee/mellobit/com/teamworksample/projectlist/ProjectListFragment.java
+++ app/src/main/java/garrymckee/mellobit/com/teamworksample/projectlist/ProjectListFragment.java
@@ -31,11 +31,6 @@ public class ProjectListFragment extends Fragment implements ProjectListContract
     @BindView(R.id.project_list_view)
     RecyclerView mProjectListView;
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
